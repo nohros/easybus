e5499b100540f44c29f8d891b9528537c4f2edd5
Neylor Ohmaly <nohros.ceo@gmail.com>
2013-10-19 17:32:24 -0300

    Initial forlder structure

---
+++ src/web/html/src/app/app.js
@@ -0,0 +1,25 @@
+angular.module( 'easyBus', [
+  'templates-app',
+  'templates-common',
+  'easyBus.home',
+  'easyBus.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | EasyBus' ;
+    }
+  });
+})
+
+;
+
